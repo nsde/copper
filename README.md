d36b78771aeb8001352c2c742c31547fdf08a3e8
nsde <neostyxde@gmail.com>
2021-01-10 13:52:20 +0100

    commit

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-![Icon](icon_small.png)
+![Icon](src/icon_small.png)
 # Copper Antivirus
 ## protect your computer against fake virus alerts and tech scams.
--- main/main.py
+++ main/main.py
@@ -1,1 +1,29 @@
+import os
+import tkinter
+import infi.systray
 
+# Important variables
+cwd = os.getcwd()
+
+# GUI
+win = tkinter.Tk()
+win.title("copper - Dashboard")
+win.config(bg="#141414")
+win.geometry("500x400")
+win.iconphoto(False, tkinter.PhotoImage(file=cwd + "\\src\\icon_small.png"))
+
+# Tray
+def say_hello(systray):
+    print("Hello, World!")
+
+tray_options = (("Say Hello", None, say_hello),)
+systray = infi.systray.SysTrayIcon(cwd + "\\src\\favicon.ico", "copper", tray_options)
+systray.start()
+
+# API
+with open (cwd + "\\config\\token.txt") as f:
+    token = f.read()
+print(token)
+
+# Mainloop
+win.mainloop()
